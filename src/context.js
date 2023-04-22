676a8b330a139c8a2f7458eae5e3a8b8ceb33992
Sudip <xthazudp@gmail.com>
2023-04-22 10:54:56 +0545

    create context

--- src/context.js
+++ src/context.js
@@ -1,1 +1,9 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, createContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppProvider };
